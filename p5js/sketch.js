0e1a9c9475b4690962240d98acbea40f5fd1cef6
PzanettiD <zanetti.pepe@gmail.com>
2019-12-21 00:05:50 -0300

    fixing randomization in p5js

--- p5js/sketch.js
+++ p5js/sketch.js
@@ -1,10 +1,14 @@
 function setup()
 {
 	createCanvas(800, 800);
-	stroke(0);
 	fill(0);
 	strokeWeight(6);
-	
+	c0 = color(255, 255, 255);
+	c1 = color(0, 0, 0);
+	c2 = color(255, 0, 255);
+	c3 = color(0, 255, 255);
+	canvas.style = "";
+
 	//Line 1 (Horizontal)
 	x = width/2 - 6;
 	y = 0; //randomization	
@@ -14,7 +18,21 @@ function setup()
 	y1 = height/2 - 6;
 	
 	//Ellipses radius
-	r_1 = 50;
+	r_1 = 100;
+	
+}
+
+function randomization()
+{
+	if (mouseIsPressed)
+	{
+		x1 = random(-width/2 + 6, width/2 - 6);
+		y = random(-height/2 + 6, height/2 - 6)
+		c0 = color(random(0,255), random(0, 255), random(0,255));
+		c1 = color(random(0,255), random(0, 255), random(0,255));
+		c2 = color(random(0,255), random(0, 255), random(0,255));
+		c3 = color(random(0,255), random(0, 255), random(0,255));
+	} 
 }
 
 
@@ -22,36 +40,40 @@ function draw()
 {	
 	background(255);
 	translate(width/2, height/2);
+	randomization();
 	
 	//Line 1 (Horizontal)
+	stroke(random(0, 255), random(0, 255), random(0, 255));
 	line(x, y, -x, y);
 
 	//Line 2 (Vertical)
 	line(x1, y1, x1, -y1);
 
 	//1st Quadrant
-	fill(255, 0, 0);
+	fill(c0);
 	rect(x1, y, -x - x1, -y1 - y);
-	circle((-x - x1)/2 + x1, (-y1 - y)/2 + y, r_1);
+	filter(INVERT);
+	circle((-x - x1)/2 + x1, (-y1 - y)/2 + y, random(0, 200));
+	filter(INVERT);
 
 	//2nd Quadrant
-	fill(0, 255, 0);
+	fill(c1);
 	rect(x1, y, x - x1, -y1 - y);
-	circle((x - x1)/2 + x1, (-y1 - y)/2 + y, r_1);
-	
+	filter(INVERT);
+	circle((x - x1)/2 + x1, (-y1 - y)/2 + y, random(0, 200));
+	filter(INVERT);
+
 	//3rd Quadrant
-	fill(0, 0, 255);
+	fill(c2);
 	rect(x1, y, -x - x1, y1 - y)
-	circle((-x - x1)/2 + x1, (y1 - y)/2 + y, r_1);
+	filter(INVERT);
+	circle((-x - x1)/2 + x1, (y1 - y)/2 + y, random(0, 200));
+	filter(INVERT);
 
 	//4th Quadrant
-	fill(255, 255, 0);
+	fill(c3);
 	rect(x1, y, x - x1, y1 - y)
-	circle((x - x1)/2 + x1, (y1 - y)/2 + y, r_1);
-
-	if (mouseIsPressed)
-	{
-		x1 = random(-width/2 + 6, width/2 - 6);
-		y = random(-height/2 + 6, height/2 - 6)
-	} 
+	filter(INVERT);
+	circle((x - x1)/2 + x1, (y1 - y)/2 + y, random(0, 200));
+	filter(INVERT);
 }
